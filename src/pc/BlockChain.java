5709d62883668ed710a0e223685b8df6ef4d15e8
Khuat <khuatgia@hermann.cs.grinnell.edu>
2019-02-27 16:45:53 -0600

    hello

--- src/pc/BlockChain.java
+++ src/pc/BlockChain.java
@@ -11,11 +11,8 @@ public class BlockChain {
 
 
 	public BlockChain(int initial) throws Exception{
-		byte[] bt = new byte[] { (byte)0xe0, 0x4f, (byte)0xd0,
-			    0x20, (byte)0xea, 0x3a, 0x69, 0x10, (byte)0xa2, (byte)0xd8, 0x08, 0x00, 0x2b,
-			    0x30, 0x30, (byte)0x9d };
 		this.num = 1;
-		Block b = new Block(0, initial, new Hash(bt));
+		Block b = new Block(0, initial, null);
 		this.first = new Node(b, null);
 		this.last = this.first;
 		this.remainingBal = initial;
